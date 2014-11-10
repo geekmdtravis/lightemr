79de458c2e985d20d4f5d920ec66f7cee570c200
tnesbit450 <tnesbit450@gmail.com>
2014-11-09 19:09:28 -0800

    SQLite3 implementatin into the program initiated. Can now create a generic table which will represent a patient data structure.

--- src/dbtest.c
+++ src/dbtest.c
@@ -1,14 +1,104 @@
 // file: dbtest.c
 
+// TO DO: Create a function which uses
+// strcat(dest, src) to integrate user input
+// or other data into an SQL query.
+// E.g. "INSERT INTO %s VALUES (%s, %s, ..., %s)
+// where %s can be some user input value.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 #include "patient.h"
 #include "database.h"
+
+int Table_create(sqlite3 *db);
+void Database_validate(int rc);
+void Patient_data_input(Patient *p);
 
 int main()
 {
   Patient *p = Patient_create();
+  sqlite3 *db;
+  char *sqlError = "Sqlite3 ERROR.";
+  int rc;
+
+  Patient_data_input(p);
+  // Patient_print_info(p);
+
+  rc = sqlite3_open("patient.db", &db);
+  Database_validate(rc);
+
+  Table_create(db);
+  
+  sqlite3_close(db);
+
+  exit(EXIT_SUCCESS);
+}
+
+int Table_create(sqlite3 *db)
+{
+  int rc;
+  char *error = "ERROR";
+  
+  char *sql = "CREATE TABLE PATIENTS(" \
+    "MRN INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " \
+    "FIRST CHAR(40) NOT NULL, " \
+    "MIDDLE CHAR(40), " \
+    "LAST CHAR(40) NOT NULL, " \
+    "MONTH INT NOT NULL, " \
+    "DAY INT NOT NULL, " \
+    "YEAR INT NOT NULL, " \
+    "ADDR_F1 CHAR(50) NOT NULL, " \
+    "ADDR_F2 CHAR(50) NOT NULL, " \
+    "ADDR_F3 CHAR(50) NOT NULL, " \
+    "ADDR_F4 CHAR(50) NOT NULL, " \
+    "PHONE_H CHAR(40), " \
+    "PHONE_C CHAR(40), " \
+    "PHONE_W CHAR(40), " \
+    "EMAIL CHAR(50), " \
+    "EC1_FULLNAME CHAR(80) NOT NULL, " \
+    "EC1_RELATIONSHIP CHAR(100) NOT NULL, " \
+    "EC1_PHONE_H CHAR(40), " \
+    "EC1_PHONE_W CHAR(40), " \
+    "EC1_PHONE_C CHAR(40), " \
+    "EC1_EMAIL CHAR(50), " \
+    "EC2_FULLNAME CHAR(80) NOT NULL, " \
+    "EC2_RELATIONSHIP CHAR(100) NOT NULL, " \
+    "EC2_PHONE_H CHAR(40), " \
+    "EC2_PHONE_W CHAR(40), " \
+    "EC2_PHONE_C CHAR(40), " \
+    "EC2_EMAIL CHAR(50), " \
+    "PID CHAR(40)" \
+    ");";
+
+  rc = sqlite3_exec(db, sql, NULL, 0, &error);
+
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL Error: %s\n", error);
+  } else {
+    fprintf(stdout, "Table created.\n");
+  }
+  
+    
+    
+  return 0;
+}
+
+void Database_validate(int rc)
+{
+  #define RETURN_SUCCESS 0
+  
+  if(rc == RETURN_SUCCESS) {
+    fprintf(stdout, "Opened database successfully.\n");
+  } else {
+    fprintf(stderr, "Could not open database.\n");
+  }
+}
+
+void Patient_data_input(Patient *p)
+{
   p->init = Patient_populate;
   Name n = {.first = "Travis", .middle = "Robert", .last = "Nesbit" };
   Birthdate b = {.month = 6, .day = 29, .year = 1983};
@@ -31,10 +121,7 @@ int main()
 			   .contact.phone_w = "",
 			   .contact.email = "cashewsmama@gmail.com" };
    p->init(p, &n, &b, &a, &c, &ec1, &ec2, "111222333", "1234567890");
-
-   Patient_print_info(p);
-
-  exit(EXIT_SUCCESS);
+  
 }
 
 // eof: dbtest.c
