815471a1ce1c65d14ca10c0fc93a05e37d2a7e5a
tnesbit450 <tnesbit450@gmail.com>
2014-11-11 21:08:00 -0800

    Updated test patient.

--- src/dbtest.c
+++ src/dbtest.c
@@ -108,24 +108,24 @@ void Patient_data_input(Patient *p)
   p->init = Patient_populate;
   Name n = {.first = "Travis", .middle = "Robert", .last = "Nesbit" };
   Birthdate b = {.month = 6, .day = 29, .year = 1983};
-  Address a = {.field1 = "field1",
-	       .field2 = "field2",
-	       .field3 = "field3",
-	       .field4 = "field4" };
+  Address a = {.field1 = "1 Glenhurst",
+	       .field2 = "Irvine, CA",
+	       .field3 = "92604-1234",
+	       .field4 = "United States of America" };
   Contact c = {.phone_h = "9518138895", .phone_c = "9518138895",
-	       .phone_w = "7144567890", .email = "tnesbit450ATgmail.com" };
+	       .phone_w = "7144567890", .email = "tnesbit450@gmail.com" };
   Emergency_contact ec1 = { .full_name = "Jennifer T. Baker-Nesbit",
 			   .relationship = "Wife",
 			   .contact.phone_h = "9512970019",
-			   .contact.phone_c = "9512970019",
-			   .contact.phone_w = "",
-			   .contact.email = "cashewsmamaATgmail.com" };
-   Emergency_contact ec2 = { .full_name = "Jennifer T. Baker-Nesbit",
-			   .relationship = "Wife",
-			   .contact.phone_h = "9512970019",
-			   .contact.phone_c = "9512970019",
+			   .contact.phone_c = "",
 			   .contact.phone_w = "",
 			   .contact.email = "cashewsmama@gmail.com" };
+  Emergency_contact ec2 = { .full_name = "Mary E. Nesbit-Houx",
+			   .relationship = "Mother",
+			   .contact.phone_h = "9516955456",
+			   .contact.phone_c = "",
+			   .contact.phone_w = "",
+			   .contact.email = "mary.houx@gmail.com" };
    p->init(p, &n, &b, &a, &c, &ec1, &ec2, "111222333", "1234567890");
   
 }
