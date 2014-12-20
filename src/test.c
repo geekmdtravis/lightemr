5831fa9f27a4fe9e8ff28cb334019be7c854665c
tnesbit450 <tnesbit450@gmail.com>
2014-12-19 17:20:30 -0800

    Working on the linked list datastructure for the notes.

--- src/test.c
+++ src/test.c
@@ -4,9 +4,27 @@
 #include "database.h"
 #include "patient.h"
 
+/*****************************
+ * Similar to PQ_node, but for notes.
+ ****************************/
+typedef struct Note_query_node {
+  int count;
+  BOOL selected;
+  Note *note;
+  struct Note_query_node *next;
+  struct Note_query_node *prev;
+} NQ_node;
+  
+
+NQ_node *NQ_node_alloc(void);
 Note *Note_lookup(sqlite3 *db, char *identifier, char *field);
 char *Create_note_lookup_query(char *identifier, char *field);
 int Note_find_callback(void *udp, int c_num, char *c_vals[], char *c_names[]);
+NQ_node *NQ_list_find_tail(NQ_node *n);
+BOOL NQ_node_add(NQ_node *n);
+BOOL NQ_list_pop(NQ_node *n);
+BOOL NQ_list_purge(NQ_node *n);
+BOOL NQ_node_destroy(NQ_node *n);
 
 int main()
 {
@@ -43,6 +61,7 @@ int main()
   // Lookup the note from the database
   n = Note_lookup(db, "1", "MRN");
 
+  // Print the results of the lookup
   fprintf(stdout,
 	  "Patient MRN: %s\n"
 	  "Author: %s     Title : %s\n     Time  : %s\n"
@@ -55,6 +74,7 @@ int main()
 	  n->time,
 	  n->text);
 
+  // Cleanup and exit
   if(error) sqlite3_free(error); error = NULL;
   if(query) free(query); query = NULL;
   if(n) Note_destroy(n); n = NULL;
@@ -65,27 +85,31 @@ int main()
 
 Note *Note_lookup(sqlite3 *db, char *identifier, char *field)
 {
-  Note *n = Note_create("NULL");
+  Note *n = NULL;
+  NQ_node *n_head = NQ_node_alloc();
   char *sqlQuery = Create_note_lookup_query(identifier, field);
   char *error = NULL;
   int rc;
 
-  rc = sqlite3_exec(db, sqlQuery, Note_find_callback,  n, &error);
+  rc = sqlite3_exec(db, sqlQuery, Note_find_callback,  n_head, &error);
+  n = n_head->note;
   if(sqlQuery) free(sqlQuery); sqlQuery = NULL;
   if(error) sqlite3_free(error); error = NULL;
+  // if(n_head) NQ_list_purge(n_head);
 
   return n;
 }
 
 int Note_find_callback(void *udp, int c_num, char *c_vals[], char *c_names[])
 {
-  strcpy(((Note*)udp)->mrn, c_vals[0]);
-  strcpy(((Note*)udp)->title, c_vals[1]);
-  strcpy(((Note*)udp)->author, c_vals[2]);
-  strcpy(((Note*)udp)->time, c_vals[3]);
-  ((Note*)udp)->time_sec = atoi(c_vals[4]);
-  strcpy(((Note*)udp)->replaced, c_vals[5]);
-  strcpy(((Note*)udp)->text, c_vals[6]);
+  
+  strcpy(((NQ_node*)udp)->note->mrn, c_vals[0]);
+  strcpy(((NQ_node*)udp)->note->title, c_vals[1]);
+  strcpy(((NQ_node*)udp)->note->author, c_vals[2]);
+  strcpy(((NQ_node*)udp)->note->time, c_vals[3]);
+  ((NQ_node*)udp)->note->time_sec = atoi(c_vals[4]);
+  strcpy(((NQ_node*)udp)->note->replaced, c_vals[5]);
+  strcpy(((NQ_node*)udp)->note->text, c_vals[6]);
   
   return 0;
 }
@@ -105,4 +129,52 @@ char *Create_note_lookup_query(char *identifier, char *field)
   strcat(query, "\");");	 
   
   return query;
+}
+
+NQ_node *NQ_node_alloc()
+{
+  NQ_node *n = malloc(sizeof(NQ_node));
+  n->next = NULL;
+  n->prev = NULL;
+  n->count = 1;
+  n->selected = FALSE;
+
+  n->note = Note_create("NULL");  
+
+  return n;
+}
+
+NQ_node *NQ_list_find_tail(NQ_node *n)
+{
+  NQ_node *tail;
+
+  for (tail = n; tail->next; tail = tail->next);
+
+  return tail;
+}
+
+BOOL NQ_node_add(NQ_node *n)
+{
+  NQ_node *tail = NQ_list_find_tail(n);
+  NQ_node *new = malloc(sizeof(NQ_node));
+  new->next = NULL;
+  new->prev = tail;
+  new->count = tail->count + 1;
+  
+  return ((new) ? TRUE : FALSE);
+}
+
+BOOL NQ_list_pop(NQ_node *n)
+{
+  return TRUE;
+}
+
+BOOL NQ_list_purge(NQ_node *n)
+{
+  return TRUE;
+}
+
+BOOL NQ_node_destroy(NQ_node *n)
+{
+  return TRUE;
 }
