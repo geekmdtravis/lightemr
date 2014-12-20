5908af43834be1ad7840b15958516b0b2b2bd8df
tnesbit450 <tnesbit450@gmail.com>
2014-12-19 16:41:09 -0800

    Having working note lookup; only returning tail of note, so need to create linked list structure again.

--- src/test.c
+++ src/test.c
@@ -6,6 +6,7 @@
 
 Note *Note_lookup(sqlite3 *db, char *identifier, char *field);
 char *Create_note_lookup_query(char *identifier, char *field);
+int Note_find_callback(void *udp, int c_num, char *c_vals[], char *c_names[]);
 
 int main()
 {
@@ -42,6 +43,18 @@ int main()
   // Lookup the note from the database
   n = Note_lookup(db, "1", "MRN");
 
+  fprintf(stdout,
+	  "Patient MRN: %s\n"
+	  "Author: %s     Title : %s\n     Time  : %s\n"
+	  "Note contents\n"
+	  "=============\n"
+	  "%s\n",
+	  n->mrn,
+	  n->author,
+	  n->title,
+	  n->time,
+	  n->text);
+
   if(error) sqlite3_free(error); error = NULL;
   if(query) free(query); query = NULL;
   if(n) Note_destroy(n); n = NULL;
@@ -52,16 +65,29 @@ int main()
 
 Note *Note_lookup(sqlite3 *db, char *identifier, char *field)
 {
-  Note *n = NULL;
+  Note *n = Note_create("NULL");
   char *sqlQuery = Create_note_lookup_query(identifier, field);
   char *error = NULL;
   int rc;
 
-  rc = sqlite3_exec(db, sqlQuery, NULL, NULL, &error);
+  rc = sqlite3_exec(db, sqlQuery, Note_find_callback,  n, &error);
   if(sqlQuery) free(sqlQuery); sqlQuery = NULL;
   if(error) sqlite3_free(error); error = NULL;
 
   return n;
+}
+
+int Note_find_callback(void *udp, int c_num, char *c_vals[], char *c_names[])
+{
+  strcpy(((Note*)udp)->mrn, c_vals[0]);
+  strcpy(((Note*)udp)->title, c_vals[1]);
+  strcpy(((Note*)udp)->author, c_vals[2]);
+  strcpy(((Note*)udp)->time, c_vals[3]);
+  ((Note*)udp)->time_sec = atoi(c_vals[4]);
+  strcpy(((Note*)udp)->replaced, c_vals[5]);
+  strcpy(((Note*)udp)->text, c_vals[6]);
+  
+  return 0;
 }
 
 char *Create_note_lookup_query(char *identifier, char *field)
@@ -72,7 +98,7 @@ char *Create_note_lookup_query(char *identifier, char *field)
   for (i = 0; i < MAX_QUERY; ++i) query[i] = '\0';
 
   // SELECT * FROM NOTES WHERE LOWER("MRN") = LOWER("1"); 
-  strcat(query, "SELECT * FROM PATIENTS WHERE LOWER(\"");
+  strcat(query, "SELECT * FROM NOTES WHERE LOWER(\"");
   strcat(query, field);
   strcat(query, "\") = LOWER(\"");
   strcat(query, identifier);
